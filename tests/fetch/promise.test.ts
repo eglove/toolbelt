35d141d353ca9ae21f476de8d1ac52200a2f1b03
Ethan Glover <hello@ethang.email>
2024-02-17 14:47:46 -0600

    remove / 2

--- tests/fetch/promise.test.ts
+++ tests/fetch/promise.test.ts
@@ -50,7 +50,7 @@ describe('promiseAllSettled', () => {
     });
     const all = performance.now() - startAll;
 
-    expect(all).toBeLessThan(sequential / 2);
+    expect(all).toBeLessThan(sequential);
   });
 });
 
