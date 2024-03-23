cacdc4087d580e5a513994ff4757ffb86c318b21
Ethan Glover <hello@ethang.email>
2024-03-23 11:02:08 -0500

    Update ZodValidator to support ZodRecord type
    The ZodValidator is updated to also include the ZodRecord type in addition to existing types. This enlargement will help in validating more complex data types. A type of such record consists of strings which increases the type's versatility.

--- src/types/zod-validator.ts
+++ src/types/zod-validator.ts
@@ -2,4 +2,5 @@ import type { z } from 'zod';
 
 export type ZodValidator =
   | z.ZodArray<z.ZodObject<z.ZodRawShape>>
-  | z.ZodObject<z.ZodRawShape>;
+  | z.ZodObject<z.ZodRawShape>
+  | z.ZodRecord<z.ZodString, z.ZodString>;
