f7eec7a69e77300a6ae47b6dc598d8076a3e9f11
Ethan <hello@ethang.email>
2024-08-13 08:14:17 -0500

    lint

--- build.mjs
+++ build.mjs
@@ -2,8 +2,8 @@ import { projectBuilder } from "@ethang/project-builder/project-builder.js";
 
 await projectBuilder("toolbelt", "main", {
   isLibrary: true,
-  scripts: ["UPDATE", "DEDUPE", "LINT", "TEST"],
   publishDirectory: "dist",
+  scripts: ["UPDATE", "DEDUPE", "LINT", "TEST"],
   tsConfigOverrides: {
     compilerOptions: {
       emitDeclarationOnly: true,
