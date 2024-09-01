69424527e7c9c808e5b99761c7781cb12b921426
Ethan <hello@ethang.email>
2024-08-31 20:33:54 -0500

    fix attempt async

--- src/functional/attempt-async.ts
+++ src/functional/attempt-async.ts
@@ -1,12 +1,15 @@
 import isError from "lodash/isError.js";
 
-export const attemptAsync = async <T extends () => unknown>(
+export const attemptAsync = async <
+  T extends (...parameters: Parameters<T>) => unknown,
+>
+(
   callback: T,
+  ...parameters: Parameters<T>
 ): Promise<Awaited<ReturnType<T>> | Error> => {
   try {
     // Assume this is used with async function, force the await to catch the error
-
-    return (await callback()) as Awaited<ReturnType<T>>;
+    return (await callback(...parameters)) as Awaited<ReturnType<T>>;
   } catch (error: unknown) {
     return isError(error)
       ? error
