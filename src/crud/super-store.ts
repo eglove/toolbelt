96452da358e21289b76009d3173bc01ad6bcc453
Ethan <hello@ethang.email>
2024-09-09 21:16:19 -0500

    Add SuperStore class and update dependencies
    Added a new `SuperStore` class implementation in `src/crud/super-store.ts` with methods for managing state. Updated `package.json` dependencies to include `@tanstack/store` and `immer`, and reflected these additions in the `pnpm-lock.yaml` file.

---
+++ src/crud/super-store.ts
@@ -0,0 +1,58 @@
+import type { Get, StringKeyOf } from "type-fest";
+
+import { type AnyUpdater, Store, type StoreOptions } from "@tanstack/store";
+import { produce } from "immer";
+import get from "lodash/get.js";
+import set from "lodash/set.js";
+
+type BaseRecord = Record<number | string | symbol, unknown>;
+
+type NestedPath<T extends BaseRecord> = {
+  [K in StringKeyOf<T>]: T[K] extends BaseRecord
+    ? [K, ...NestedPath<T[K]>] | [K]
+    : [K]
+}[StringKeyOf<T>];
+
+export class SuperStore<
+  TState extends BaseRecord,
+  TUpdater extends AnyUpdater = (callback: TState) => TState,
+> {
+  private readonly _initialState: TState;
+
+  public readonly store: Store<TState, TUpdater>;
+
+
+  public constructor(
+    initialState: TState, options?: StoreOptions<TState, TUpdater>,
+  ) {
+    this._initialState = initialState;
+    this.store = new Store(initialState, options);
+  }
+
+
+  public getValue<Path extends NestedPath<TState>>(
+  // @ts-expect-error allow deeply nested type
+    path: Path, fallback?: Get<TState, Path>,
+  ) {
+    return get(this.store.state, path, fallback) as Get<TState, Path>;
+  }
+
+  public resetState() {
+    this.store.setState((() => {
+      return this._initialState;
+    }) as TUpdater);
+  }
+
+  public setValue<Path extends NestedPath<TState>>(
+    path: Path,
+    value: Get<TState, Path>,
+  ) {
+    const produced = produce(this.store.state, (draft) => {
+      set(draft, path, value);
+    });
+
+    this.store.setState((() => {
+      return produced;
+    }) as TUpdater);
+  }
+}
